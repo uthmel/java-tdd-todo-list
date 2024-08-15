a2a446f779fa7ba70f954d10952eb071e8fda584
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-15 10:21:03 +0200

    added testAddSecondTask

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -16,8 +16,17 @@ class TodoListTest {
     @Test
     public void testAddTaskIfExists() {
         TodoList todolist = new TodoList();
+        todolist.addTask("Buy groceries");
         boolean result2 = todolist.addTask("Buy groceries");
         Assertions.assertFalse(result2, "The task should not be added successfully, since it already exists");
 
     }
+
+    @Test
+    public void testAddSecondTask() {
+        TodoList todoList = new TodoList();
+        Assertions.assertTrue(todoList.addTask("Buy Clothes"));
+    }
+
+
 }
