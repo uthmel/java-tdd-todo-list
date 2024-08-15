534508f8800541b22e62453f31ac225a6bec3c40
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-15 10:11:00 +0200

    added source code for the to first tests

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,41 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoList {
+    private final List<Task> tasks;
+
+    public TodoList(){
+        this.tasks = new ArrayList<>();
+    }
+
+
+    public boolean addTask(String taskDescription){
+        for(Task task : tasks){
+            if (task.getDescription().equalsIgnoreCase(taskDescription)){
+                return false;
+            }
+        }
+
+        Task newTask = new Task(taskDescription);
+        tasks.add(newTask);
+        return true;
+    }
+
+}
+
+
+class Task {
+    private final String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 
 }
