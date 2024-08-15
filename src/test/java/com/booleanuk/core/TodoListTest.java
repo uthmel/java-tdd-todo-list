8cf10894702f3842529781deeca60e197b7ca877
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-15 09:44:35 +0200

    added testAddTask

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoListTest {
+
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void testAddTask(){
+        TodoList todolist = new TodoList();
+        boolean result1 = todolist.addTask("Buy groceries");
+        Assertions.assertTrue(result1, "The task should be added sucessfully");
     }
 }
